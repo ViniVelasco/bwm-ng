7c8a66fa388c23f1ea060ba9f469800affa37626
= <sasukegc@gmail.com>
2018-12-24 14:00:47 -0200

    Adds proxy configuration, adds rental details styles improvement, and adds pipes

--- src/app/rental/shared/rental.service.ts
+++ src/app/rental/shared/rental.service.ts
@@ -1,80 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Rental } from './rental.model'
+import { HttpClient } from '@angular/common/http'
 
 @Injectable()
 export class RentalService {
-  private rentals: Rental[] = [{
-    id: "1",
-    title: "Central Apartment",
-    city: 'New York',
-    street: 'Times Sequare',
-    category: "apartment",
-    image: 'http:via.placeholder.com/350x250',
-    bedrooms: 3,
-    description: "Very nice apartment",
-    dailyRate: 34,
-    shared: false,
-    createdAt: '24/12/2017'
-  },
-  {
-    id: "2",
-    title: "Central Apartment",
-    city: 'New York',
-    street: 'Times Sequare',
-    category: "apartment",
-    image: 'http:via.placeholder.com/350x250',
-    bedrooms: 3,
-    description: "Very nice apartment",
-    dailyRate: 34,
-    shared: false,
-    createdAt: '24/12/2017'
-  },
-  {
-    id: "3",
-    title: "Central Apartment",
-    city: 'New York',
-    street: 'Times Sequare',
-    category: "apartment",
-    image: 'http:via.placeholder.com/350x250',
-    bedrooms: 3,
-    description: "Very nice apartment",
-    dailyRate: 34,
-    shared: false,
-    createdAt: '24/12/2017'
-  },
-  {
-    id: "4",
-    title: "Central Apartment",
-    city: 'New York',
-    street: 'Times Sequare',
-    category: "apartment",
-    image: 'http:via.placeholder.com/350x250',
-    bedrooms: 3,
-    description: "Very nice apartment",
-    dailyRate: 34,
-    shared: false,
-    createdAt: '24/12/2017'
-  }]
 
-  public getRentalById(rentalId: string): Observable<Rental> {
-    return new Observable<Rental>((observer) => {
+  constructor(private http : HttpClient) {}
 
-      setTimeout(() => {
-        const foundRental = this.rentals.find((rental) => {
-          return rental.id == rentalId
-        })
-
-        observer.next(foundRental)
-      }, 500)
-    });
+  public getRentalById(rentalId: string): Observable<any> {
+    return this.http.get('/api/v1/rentals/' + rentalId)
   }
   
-  public getRentals(): Observable<Rental[]> {
-    return new Observable<Rental[]>((observer) => {
-      setTimeout(() => {
-        observer.next(this.rentals);
-      }, 1000);
-    });
+  public getRentals(): Observable<any> {
+    return this.http.get('/api/v1/rentals');
   }
 }

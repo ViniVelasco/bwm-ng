eebd3cee34bce9f3075e84fe86fc6e2d0359cebd
= <sasukegc@gmail.com>
2018-12-25 15:24:40 -0200

    Add map component and google api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
 
-import { RentalModule } from './rental/rental.module'
+import { RentalModule } from './rental/rental.module';
 
 const routes: Routes = [
   { path: '', redirectTo: '/rentals', pathMatch: 'full'}

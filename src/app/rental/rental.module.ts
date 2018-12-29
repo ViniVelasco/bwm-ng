eb683988898603ca1779771517d72c53e382c266
= <sasukegc@gmail.com>
2018-12-29 15:42:56 -0200

    Adds booking creation functionality

--- src/app/rental/rental.module.ts
+++ src/app/rental/rental.module.ts
@@ -2,19 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
 import { NgPipesModule } from 'ngx-pipes';
 import { MapModule } from '../common/map/map.module';
 import { Daterangepicker } from 'ng2-daterangepicker';
+import { UppercasePipe } from '../common/pipes/uppercase.pipe';
 
 import { RentalListItemComponent } from './rental-list-item/rental-list-item.component';
 import { RentalListComponent } from './rental-list/rental-list.component';
 import { RentalComponent } from './rental.component';
-import { RentalService } from './shared/rental.service';
 import { RentalDetailComponent } from './rental-detail/rental-detail.component';
-
-import { UppercasePipe } from '../common/pipes/uppercase.pipe';
 import { AuthGuard } from '../auth/shared/auth.guard';
 import { RentalDetailBookingComponent } from './rental-detail/rental-detail-booking/rental-detail-booking.component';
+
+import { HelperService } from '../common/service/helper.service';
+import { RentalService } from './shared/rental.service';
+import { BookingService } from '../booking/shared/booking.service';
 
 const routes: Routes = [
   { path: 'rentals', 
@@ -41,8 +44,13 @@ const routes: Routes = [
     HttpClientModule,
     NgPipesModule,
     MapModule,
-    Daterangepicker
+    Daterangepicker,
+    FormsModule
   ],
-  providers: [RentalService]
+  providers: [
+    RentalService, 
+    HelperService,
+    BookingService
+  ]
 })
 export class RentalModule {}
